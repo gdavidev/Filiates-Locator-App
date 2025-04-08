f6ad8e813507f9c6da86d925953068b066bad6ef
David <77506410+gdavidev@users.noreply.github.com>
2025-04-07 22:48:59 -0300

    ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+        {
+            name: "full-app",
+            script: "npm",
+            args: "start",
+            interpreter: "none",
+            watch: false
+        }
+    ]
+};
